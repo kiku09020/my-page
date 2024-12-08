1bda678e76a6bbdee264c2c5c642054d35a005ae
KK09020 <kiku09020@gmail.com>
2024-12-09 08:27:18 +0900

    reCAPTCHAの実装

--- src/pages/contact/components/Racaptcha.tsx
+++ src/pages/contact/components/Racaptcha.tsx
@@ -1,13 +1,18 @@
 import { createRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
+const recaptchaRef = createRef<ReCAPTCHA>();
+
 export default function Recaptcha() {
-  const siteKey = `${import.meta.env.REACT_APP_RECAPTCHA_SITE_KEY}`;
-  const recaptchaRef = createRef<ReCAPTCHA>();
+  const siteKey = `${import.meta.env.VITE_RECAPTCHA_SITE_KEY}`;
 
-  const onSubmit = () => {
-    recaptchaRef.current?.execute();
-  };
-
-  return <ReCAPTCHA ref={recaptchaRef} sitekey={siteKey} />;
+  return <ReCAPTCHA ref={recaptchaRef} sitekey={siteKey} size="invisible" />;
 }
+
+export const executeRecaptcha = async () => {
+  try {
+    await recaptchaRef.current?.executeAsync();
+  } catch (e) {
+    console.error(e);
+  }
+};
--- src/pages/contact/components/form/Form.tsx
+++ src/pages/contact/components/form/Form.tsx
@@ -8,6 +8,7 @@ import { FromFieldProps } from "../../models/form/formField";
 import { Box, Button } from "@mui/material";
 import sendEmail from "../../../features/emailjs";
 import { useRef } from "react";
+import Recaptcha, { executeRecaptcha } from "../Racaptcha";
 
 //------------------------------------------------------------
 
@@ -25,17 +26,13 @@ export default function Form() {
     formState: { errors, isValid },
   } = useForm<FormData>({ mode: "onChange", resolver: zodResolver(formSchema) });
 
+  // スキーマのエラーメッセージのマッピング
   zodErrorMap();
 
   const onSubmit = handleSubmit(async (data: FormData) => {
-    try {
-      await sendEmail(formRef);
-      console.log("Email sent successfully");
-      alert("送信に成功しました");
-    } catch (error) {
-      console.error("Error sending email:", error);
-      alert("送信に失敗しました");
-    }
+    await executeRecaptcha(); // reCAPTCHA実行
+
+    await sendEmail(formRef);
   });
 
   return (
@@ -45,15 +42,20 @@ export default function Form() {
       onSubmit={onSubmit}
       ref={formRef}
     >
+      {/* フィールド */}
       <FormContext.Provider value={{ control, errors }}>
         {formFields.map((props) => (
           <FormField key={props.name} {...props} />
         ))}
       </FormContext.Provider>
 
+      {/* 送信ボタン */}
       <Button type="submit" variant="contained" disabled={!isValid} sx={{ m: "0 auto" }}>
         送信
       </Button>
+
+      {/* reCAPTCHA */}
+      <Recaptcha />
     </Box>
   );
 }
